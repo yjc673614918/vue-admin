75de691174ac41c80cfb04143478df8bf8e0bcca
姚建成 <yjc673614918@163.com>
2020-03-22 17:42:26 +0800

    构建项目、配置全局样式、路由、elementUI

---
+++ src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    这里是登录页
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="warning" plain>警告按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
